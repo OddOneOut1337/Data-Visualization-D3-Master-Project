35c13db9c4895b529bc503e88d5df9dac2af7c62
Daniel <daniel.esco92@gmail.com>
2018-07-31 20:49:44 +0200

    aménagement

---
+++ body/script.js
@@ -0,0 +1,8 @@
+// const canevas = d3.select('body')
+// .append('svg')
+//     .attr('width',1560)
+//     .attr('height',800);
+
+d3.json('swapiPlus.json', function(data){
+    console.log(data[0]);
+});
--- script.js
+++
@@ -1,3 +0,0 @@
-d3.json(swapiPlus.json, function(){
-
-});
