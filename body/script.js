cea30eba310252a89a137a5f8ac8bcb8301fcd85
Doun92 <daniel.esco92@gmail.com>
2018-08-05 16:24:22 +0200

    changement du dossier data

--- body/script.js
+++ body/script.js
@@ -1,8 +1,3 @@
-// const canevas = d3.select('body')
-// .append('svg')
-//     .attr('width',1560)
-//     .attr('height',800);
-
-d3.json('swapiPlus.json', function(data){
+d3.json("data/vehicules1.json", function(data) {
     console.log(data[0]);
-});
+  });
